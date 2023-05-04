df36b10bbe78abe2dea3b0d86e85fadf4c022e83
RancyChepchirchir <kosgeyrancy2@gmail.com>
2023-05-05 01:47:11 +0300

    0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-'''A script that gathers employee name completed
-tasks and total number of tasks from an API
-'''
-
-import re
+"""Python script that accesses a REST API for a given employee ID,
+returns information about his/her TODO list progress"""
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    EMPLOYEE_ID = sys.argv[1]
+    EMPLOYEE_NAME = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{:d}"
+        .format(int(EMPLOYEE_ID))).json().get("name")
+    DONE_TASKS = []
+    TOTAL_NUMBER_OF_TASKS = 0
+    r = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = emp_req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+    for task in r:
+        if (task.get("userId") == int(EMPLOYEE_ID)):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if (task.get("completed")):
+                DONE_TASKS.append(task.get("title"))
+
+    print("Employee {:s} is done with tasks({:d}/{:d}):".format
+          (EMPLOYEE_NAME, len(DONE_TASKS), TOTAL_NUMBER_OF_TASKS))
+
+    for task in DONE_TASKS:
+        print("\t {:s}".format(task))
